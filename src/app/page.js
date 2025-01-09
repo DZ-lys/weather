7a74efc5a23c1211682ddd3fb585b0b696eec899
24LP2437 24LP2437 <24LP2437@G304-CC0216.local>
2025-01-09 12:25:39 +0800

    useless

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
     <div className="flex">
       <Left />
       <Right />
-      <Weather />
     </div>
   );
 }
--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Left = () => {
   const [cities, setCities] = useState([]);
   const [searched, setSearched] = useState([]);
+  const [tempreture, setTemp] = useState("");
 
   async function getData() {
     const result = await fetch("https://countriesnow.space/api/v0.1/countries");
@@ -22,6 +23,18 @@ const Left = () => {
     });
     setSearched(filtered);
   };
+  getWeather();
+
+  async function getWeather() {
+    const result = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=2e39ce842b0a4f8c87621141250801 &q={tokyo}`
+    );
+    const weather = await result.json();
+    let report = weather.maxtemp_c;
+
+    return setTemp(report);
+  }
+
   return (
     <div className="flex justify-center items-center bg-[#f3f4f6] w-[1000px] h-[1200px]">
       <div className="left-5 top-5 absolute p-10">
@@ -31,10 +44,12 @@ const Left = () => {
           onChange={searchHandler}
         />
         {searched.length > 0 &&
-          searched.slice(0, 10).map((city) => <p>{city}</p>)}
+          searched.slice(0, 5).map((city) => <p>{city}</p>)}
       </div>
       <div className="flex justify-center items-center bg-[rgb(255,255,255,0.75)] w-[414px] h-[828px]">
-        <div></div>
+        <div>
+          <p>{tempreture}</p>
+        </div>
       </div>
     </div>
   );
